3c2e1d0cabe89e62b216ddc02e31acecc2062bfa
Nogunfnn <40021976+Nogunfnn@users.noreply.github.com>
2024-06-26 13:46:18 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "NOGULOVESCOFFEE",
+    pageTitle: "NLC",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
